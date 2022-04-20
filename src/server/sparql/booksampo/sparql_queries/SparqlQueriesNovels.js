dd0b2cf38f3660874fae4260cbe24874f967dc57
Annastiina Ahola <anna.s.ahola@gmail.com>
2022-04-20 14:44:31 +0300

    Add original language and awards to novel perspective, update novel page URL formation

--- src/server/sparql/booksampo/sparql_queries/SparqlQueriesNovels.js
+++ src/server/sparql/booksampo/sparql_queries/SparqlQueriesNovels.js
@@ -4,10 +4,21 @@ export const novelProperties = `
     {
       ?id skos:prefLabel ?prefLabel__id .
       BIND(?prefLabel__id AS ?prefLabel__prefLabel)
-      BIND(CONCAT("/${perspectiveID}/page/", REPLACE(STR(?id), "^.*\\\\/(.+)", "$1")) AS ?prefLabel__dataProviderUrl)
       BIND(?id as ?uri__id)
       BIND(?id as ?uri__dataProviderUrl)
       BIND(?id as ?uri__prefLabel)
+    }
+    UNION
+    {
+      ?id skos:prefLabel ?prefLabel__id .
+      BIND(CONCAT("/${perspectiveID}/page/", REPLACE(STR(?id), "^.*\\\\/(.+)", "$1")) AS ?prefLabel__dataProviderUrl)
+      FILTER(!CONTAINS(STR(?id), "kaunokki#"))
+    }
+    UNION
+    {
+      ?id skos:prefLabel ?prefLabel__id .
+      BIND(CONCAT("/${perspectiveID}/page/", REPLACE(STR(?id), "^.*#(.+)", "$1")) AS ?prefLabel__dataProviderUrl)
+      FILTER(CONTAINS(STR(?id), "kaunokki#"))
     }
     UNION
     {
@@ -26,5 +37,19 @@ export const novelProperties = `
       ?id kaunokki:teema ?theme__id .
       ?theme__id skos:prefLabel ?theme__prefLabel .
       FILTER(LANG(?theme__prefLabel) = "<LANG>")
+    }
+    UNION
+    {
+      ?id kaunokki:alkukieli ?originalLanguage__id .
+      BIND(?originalLanguage__id as ?originalLanguage__prefLabel)
+    }
+    UNION
+    {
+      ?id kaunokki:onPalkinto ?award__id .
+      OPTIONAL { 
+        ?award__id skos:prefLabel ?award__prefLabel_ .
+        FILTER(LANG(?award__prefLabel_) = "<LANG>")
+      }
+      BIND(COALESCE(?award__prefLabel_, ?award__id) as ?award__prefLabel) 
     }
 `
